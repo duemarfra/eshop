fffbe2cb625dffd36cffc53550c311a007350cf0
duemarfra <duemarfra@gmail.com>
2022-12-08 22:42:01 -0300

    edit number of production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "eshop-production-7696.up.railway.app"
+  config.hosts << "eshop-production-f941.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
